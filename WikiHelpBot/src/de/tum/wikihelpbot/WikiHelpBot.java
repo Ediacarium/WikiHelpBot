b32ba6132ed9ce0e74579aef9e9cfbd63f69918b
Ediacarium <alex@thole.org>
2014-11-17 00:01:06 +0100

    Refactored Code with Patterns and introduced new Functions and made WikiHelpBot createable

--- WikiHelpBot/src/de/tum/wikihelpbot/WikiHelpBot.java
+++ WikiHelpBot/src/de/tum/wikihelpbot/WikiHelpBot.java
@@ -1,12 +1,9 @@
 package de.tum.wikihelpbot;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
-import java.net.SocketException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.regex.Matcher;
@@ -23,17 +20,22 @@ import org.xml.sax.SAXException;
 
 public class WikiHelpBot {
 
-	public static final Pattern extractTextfromXMLPattern = Pattern.compile("<text xml:space=\"preserve\">(.*)</text>");
-	public static final Pattern removeTableandReferencesPattern = Pattern.compile("</table>(.*)<ol class=\"references\"",Pattern.DOTALL);
-	public static final Pattern removeHTMLTagsPattern = Pattern.compile("<.*>(.*)<.*>");
-	public static final Pattern getTablePattern = Pattern.compile("<table>(.*)</table>");
+	public static final Pattern removeHTMLTagsPattern = Pattern.compile("<[^>]*>", Pattern.DOTALL);
 	
-	private static InputStream readURL(String url){
+	public static final Pattern askingPattern = Pattern.compile("(?i)Was ist |What is |Was sind |Wer ist |What are ");
+
+	private final String wikiLangDomain;
+
+	public WikiHelpBot(String wikiLangDomain) {
+		this.wikiLangDomain = wikiLangDomain;
+	}
+
+	private InputStream readURL(String url) {
 		InputStream wikiResponse = null;
 		try {
 			URL wikiurl = new URL(url);
 			wikiResponse = wikiurl.openStream();
-			
+
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -41,124 +43,138 @@ public class WikiHelpBot {
 		}
 		return wikiResponse;
 	}
-	
-	private static String buildWikiParseURL(String topic){
-		String result;
+
+	private String buildWikiParseURL(String topic) {
+
 		try {
-			return result = "http://de.wikipedia.org/w/api.php?action=parse&format=xml&page="+URLEncoder.encode(topic,"UTF-8")+"&redirects=&noimages&section=0";
+			System.out.println("http://" + wikiLangDomain + ".wikipedia.org/w/api.php?action=parse&format=xml&page=" + URLEncoder.encode(topic, "UTF-8") + "&redirects=&noimages&section=0");
+			return "http://" + wikiLangDomain + ".wikipedia.org/w/api.php?action=parse&format=xml&page=" + URLEncoder.encode(topic, "UTF-8") + "&redirects=&noimages&section=0";
 		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+			return "http://de.wikipedia.org/w/api.php?action=parse&format=xml&page=" + topic + "&redirects=&noimages&section=0";
+
 		}
-		return "http://de.wikipedia.org/w/api.php?action=parse&format=xml&page="+topic+"&redirects=&noimages&section=0";
 	}
-	
-	private static String removeHTMLfromString(String text){
-		
-		StringBuilder modWikiPageText = new StringBuilder(text);
-		
-		int startTag = 0;
-		while((startTag = modWikiPageText.indexOf("<")) >= 0){
-			int endTag = modWikiPageText.indexOf(">", startTag);
-			
-			modWikiPageText.delete(startTag, endTag+1);
-		}
-			
-			String modWikiPageString = modWikiPageText.toString();
-			boolean replaced ;
-		do{
-			String neu = modWikiPageString.replaceAll("\n\n", "\n");
-			replaced = !neu.equals(modWikiPageString);
-			modWikiPageString = neu;
-		}while(replaced);
+
+	private String removeHTMLfromString(String text) {
+
+		String modWikiPageString = removeHTMLTagsPattern.matcher(text).replaceAll("");
+		// System.out.println(modWikiPageString);
 		return modWikiPageString;
 	}
-	
-	
-	private static String extractHTMLPageText(InputStream WikiXml) throws SAXException, IOException, ParserConfigurationException{
-		
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		Document doc = db.parse(WikiXml);
-		doc.normalize();
-		
-		NodeList textNodes = doc.getElementsByTagName("text");
-		Node textNode = textNodes.item(0);
-		if(textNode != null){
-		String textNodeText = textNode.getTextContent();
-		System.out.println(textNodeText);
-		if(textNodeText.indexOf("<div class=\"redirectMsg\">") >= 0){
-			int redirectStartIndex = textNodeText.indexOf("?title=")+7;
-			int redirectEndIndex = textNodeText.indexOf("&",redirectStartIndex);
+
+	private boolean resolveRedirection(String textNodeText) {
+
+		boolean isRedirection = textNodeText.indexOf("<div class=\"redirectMsg\">") >= 0;
+
+		if (isRedirection) {
+			int redirectStartIndex = textNodeText.indexOf("?title=") + 7;
+			int redirectEndIndex = textNodeText.indexOf("&", redirectStartIndex);
 			String redirect = textNodeText.substring(redirectStartIndex, redirectEndIndex);
 			System.out.println(redirect);
-			return extractHTMLPageText(readURL(buildWikiParseURL(redirect)));
+			textNodeText = extractHTMLPageText(readURL(buildWikiParseURL(redirect)));
 		}
-//		int indexOfTableEnd = textNodeText.lastIndexOf("</table>")+9;
-//		int indexOfReferences = textNodeText.indexOf("<ol class=\"references\"");
-//		String HTMLwikiPageText = textNodeText.substring(indexOfTableEnd,indexOfReferences >= 0?indexOfReferences:textNodeText.length());
-		
-		Matcher HTMLwikiPageTextMatcher = removeTableandReferencesPattern.matcher(textNodeText);
-		HTMLwikiPageTextMatcher.find();
-		String HTMLwikiPageText = HTMLwikiPageTextMatcher.group(1);
-		System.out.println(HTMLwikiPageText);
-		
-		String wikiPageText = removeHTMLfromString(HTMLwikiPageText);
-		if(wikiPageText.matches("[\n ]*")){
-			//return removeHTMLfromString(textNodeText.substring(0,indexOfTableEnd));
-		}
-		return wikiPageText;
-	
-		}
-		return "Wikipedia doesn't respond";
+		return isRedirection;
 	}
-	
-	public static String getWikiHelp(String msg){
-		
-		//String[] split = msg.split("Was ist");
-		
-		InputStream wikiResponseStream = readURL(buildWikiParseURL(msg));
-		try {
-			return extractHTMLPageText(wikiResponseStream);
-		} catch (SAXException e) {
-		} catch (IOException e) {
-		} catch (ParserConfigurationException e) {
-		}
-		
-		System.out.println("Got wiki response");
-		
-		String wikiLine;
-		String result = new String("");
 
-		System.out.println("done.");
-		return null;
-	}
-	
-	public static String needWikiResponse(String msg){
-		Pattern pattern = Pattern.compile("(?i)Was ist |What is |Was sind |Wer ist |What are ");
-		Matcher matcher = pattern.matcher(msg);
-		if(matcher.find()){
-			int IndexOfQMark = msg.indexOf('?');
-			int start = matcher.group().length();
-			System.out.println(msg.substring(start-1, IndexOfQMark >= 0? IndexOfQMark:msg.length()).trim());
-			return getWikiHelp(msg.substring(start-1, IndexOfQMark >= 0? IndexOfQMark:msg.length()).trim());
+	private String useTablePage(String pageText) {
+
+		String wikiTableText = pageText;
+		int indexOfSisterprojects = pageText.indexOf("<div class=\"sisterproject\"") - 20;
+
+		if (indexOfSisterprojects >= 0) {
+			String tableTextwithSeeAlso = pageText.substring(0, indexOfSisterprojects);
+			int indexOfSeeAlso = tableTextwithSeeAlso.lastIndexOf("<b>");
+			System.out.println("tableTextwithSeeAlso: " + tableTextwithSeeAlso);
+			wikiTableText = removeHTMLfromString(tableTextwithSeeAlso.substring(0, indexOfSeeAlso));
 		}
+
+		return wikiTableText;
+	}
+
+	private String extractHTMLPageText(InputStream WikiXml) {
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db;
+		try {
+			db = dbf.newDocumentBuilder();
+			Document doc;
+			doc = db.parse(WikiXml);
+
+			doc.normalize();
+
+			NodeList textNodes = doc.getElementsByTagName("text");
+			Node textNode = textNodes.item(0);
+			if (textNode != null) {
+				String textNodeText = textNode.getTextContent();
+				System.out.println(textNodeText);
+
+				if (resolveRedirection(textNodeText)) {
+					System.out.println("resolved text: " + textNodeText);
+					return textNodeText;
+				}
+
+				int indexOfTableEnd = textNodeText.lastIndexOf("</table>") + 9;
+				int indexOfReferences = textNodeText.indexOf("<ol class=\"references\"");
+				String HTMLwikiPageText = textNodeText.substring(indexOfTableEnd, indexOfReferences >= 0 ? indexOfReferences : textNodeText.length());
+				System.out.println(HTMLwikiPageText);
+
+				String wikiPageText = removeHTMLfromString(HTMLwikiPageText);
+				
+				if (wikiPageText.matches("[\n ]*")) {
+					return useTablePage(textNodeText.substring(0,indexOfTableEnd));
+				}
+				return wikiPageText;
+
+			}
+			return "Wikipedia doesn't respond";
+		} catch (SAXException e) {
+			return "SAX Exception occoured";
+		} catch (ParserConfigurationException e1) {
+			return "invalid ParserConfig";
+		} catch (IOException e) {
+			return "IOException occoured";
+		}
+	}
+
+	private String getWikiHelp(String msg) {
+		InputStream wikiResponseStream = readURL(buildWikiParseURL(msg));
+		return extractHTMLPageText(wikiResponseStream);
+	}
+
+	public String needWikiResponse(String msg) {
+		Matcher askingMatcher = askingPattern.matcher(msg);
+		if (askingMatcher.find()) {
+			int IndexOfQMark = msg.indexOf('?');
+			int start = askingMatcher.group().length();
+			System.out.println(msg.substring(start - 1, IndexOfQMark >= 0 ? IndexOfQMark : msg.length()).trim());
+			return getWikiHelp(msg.substring(start - 1, IndexOfQMark >= 0 ? IndexOfQMark : msg.length()).trim());
+		}
+		
+		
 		return msg;
 	}
-	
-	public static void main(String args[]) throws IOException{
-		
-		boolean exit = false;
-		
-		while(!exit){
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			String line = reader.readLine();
-			exit = line.equals("exit");
-			String wikiHelp = needWikiResponse(line);
-			if(wikiHelp != null){
-				System.out.println("Wikipedia says:");
-				System.out.println(wikiHelp);
-			}
-		}
-		
+
+	public static void main(String args[]) throws IOException {
+
+		// boolean exit = false;
+		WikiHelpBot helpBot = new WikiHelpBot("de");
+
+		// while(!exit){
+		// BufferedReader reader = new BufferedReader(new
+		// InputStreamReader(System.in));
+		// String line = reader.readLine();
+		// exit = line.equals("exit");
+		// String wikiHelp = helpBot.needWikiResponse(line);
+		//
+		// if(wikiHelp != null){
+		// System.out.println("Wikipedia says:");
+		// System.out.println(wikiHelp);
+		// }
+		// }
+		//
+
+		System.out.println(helpBot.needWikiResponse("Was sind Bananen"));
+		System.out.println(helpBot.needWikiResponse("Was ist Memory"));
+
 	}
 }
