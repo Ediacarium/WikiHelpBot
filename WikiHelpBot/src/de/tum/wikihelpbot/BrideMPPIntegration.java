b1fdf485e35ceef751bffb87d4a6d6df25111248
Ediacarium <alex@thole.org>
2014-11-23 00:20:57 +0100

    Added the integration class for BridgeMPP

---
+++ WikiHelpBot/src/de/tum/wikihelpbot/BrideMPPIntegration.java
@@ -0,0 +1,31 @@
+package de.tum.wikihelpbot;
+
+import bridgempp.bot.example.BridgeMPPBotExample.Message;
+
+
+/**
+ * 
+ * @author Ediacarium
+ *
+ * The WikiHelpBot integration class for BridgeMPP
+ *
+ */
+public class BrideMPPIntegration implements bridgempp.bot.example.BridgeMPPBotExample.Bot {
+
+	public static final String Name = "WikiHelpBot";
+
+	WikiHelpBot helpBot;
+
+	public BrideMPPIntegration() {
+		this.helpBot = new WikiHelpBot("de");
+	}
+
+	@Override
+	public bridgempp.bot.example.BridgeMPPBotExample.Message messageRecieved(bridgempp.bot.example.BridgeMPPBotExample.Message message) {
+
+		String wikiBotMessage = helpBot.getWikiBotWisdom(message.message);
+
+		return wikiBotMessage == null ? null : new Message(Name, helpBot.needWikiResponse(message.message));
+	}
+
+}
